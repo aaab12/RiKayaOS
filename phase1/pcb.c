25098fa69df550f074d50b02935979a1545c38b1
SoftwareDiMarca <maurizio.marcaccini@studio.unibo.it>
2019-02-23 00:28:54 +0100

    Versione finale

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -12,9 +12,12 @@ struct list_head pcbfree_h;
 // pcbFree: puntatore alla lista dei pcb free
 struct list_head* pcbFree = &pcbfree_h;
 
-/* PCB handling functions */
+/* PCB HANDLING FUNCTIONS */
 
 /* PCB free list handling functions */
+
+/* Inizializzazione della lista dei PCB liberi in modo da contenere tutti gli 
+ * elementi della pcbFree_table */
 void initPcbs() {
 	INIT_LIST_HEAD(pcbFree);
 	int i;
@@ -24,10 +27,11 @@ void initPcbs() {
   }
 }
 
+/* Inserisce il PCB puntato da p nella lista dei PCB liberi */
 void freePcb(pcb_t * p) {
-	list_head *scan = pcbFree->next;									// controllo che il PCB p non sia già nella lista pcbFree
+	list_head *scan = pcbFree->next;					// controllo che il PCB p non sia già nella lista pcbFree
 	while(scan != pcbFree) {
-		if(container_of(scan, pcb_t, p_next) == p) {					// p è già nella lista pcbFree, quindi non devo aggiungerlo
+		if(container_of(scan, pcb_t, p_next) == p) {	// p è già nella lista pcbFree, quindi non devo aggiungerlo
 			return;
 		} else {
 			scan = scan->next;
@@ -36,6 +40,8 @@ void freePcb(pcb_t * p) {
 	list_add_tail(&p->p_next, pcbFree);
 }
 
+/* Rimuove un PCB della pcbFree, inizializza i suoi campi e lo restituisce
+ * Restituisce NULL se la pcbFree è vuota */
 pcb_t* allocPcb() {
 	if(list_empty(pcbFree)) {
 		return NULL;			
@@ -48,16 +54,20 @@ pcb_t* allocPcb() {
 	}
 }
 
-/* PCB queue handling functions */
+/* PCB QUEUE HANDLING FUNCTIONS */
+
+/* Inizializza la lista dei PCB istanziando la sentinella */
 void mkEmptyProcQ(struct list_head *head) {
-  INIT_LIST_HEAD(head);
-  return;
+	INIT_LIST_HEAD(head);
 }
 
+/* Lista vuota? */
 int emptyProcQ(struct list_head *head) {
   return list_empty(head);
 }
 
+/* Inserisce il PCB puntato da p nella lista puntata da head in 
+ * base alla priorità del processo: in testa priorità più alta */
 void insertProcQ(struct list_head *head, pcb_t *p) {
 	if(emptyProcQ(head) || headProcQ(head)->priority < p->priority)		//inserisco il pcb in una lista vuota o in testa 
 		list_add(&p->p_next, head);										//a una lista di pcb tutti con priorità minore
@@ -72,23 +82,25 @@ void insertProcQ(struct list_head *head, pcb_t *p) {
 	}
 }
 
+/* Restituisce il PCB in testa alla lista puntata da head
+ * senza rimuoverlo. NULL se lista vuota */
 pcb_t *headProcQ(struct list_head *head) {
-  if(emptyProcQ(head)){
+  if(emptyProcQ(head))
     return NULL;
-  } else {
-    return container_of(head->next, pcb_t, p_next);
-  }
+  return container_of(head->next, pcb_t, p_next);
 }
 
+/* Inizializza la lista dei PCB istanziando la sentinella */
 pcb_t *removeProcQ(struct list_head *head) {
 	if(emptyProcQ(head))
 		return NULL;
-	// la lista con sentinella head ha almeno un elemento
   	pcb_t* p = headProcQ(head);		//puntatore al primo pcb della lista
     list_del(&p->p_next);			//eliminazione del pcb dalla lista
-    return p;						//restituzione
+    return p;						//restituzione dello stesso
 }
 
+/* Rimuove il PCB puntato da p dalla coda puntata da head
+ * NULL se lista vuota. p in posizione qualunque nella lista */
 pcb_t *outProcQ(struct list_head *head, pcb_t *p) {
   if(emptyProcQ(head)) {
     return NULL;	//lista vuota
@@ -105,16 +117,21 @@ pcb_t *outProcQ(struct list_head *head, pcb_t *p) {
   }
 }
 
-/* Tree view functions */
-int emptyChild(pcb_t *this) {
-	return(list_empty(&this->p_child));
+/* TREE VIEW FUNCTIONS */
+
+/* TRUE se il PCB p non ha figli */
+int emptyChild(pcb_t *p) {
+	return(list_empty(&p->p_child));
 }
 
+/* Inserisce il PCB p come ultimo figlio del PCB prnt */
 void insertChild(pcb_t *prnt, pcb_t *p) {
-		p->p_parent = prnt;
-		list_add_tail(&(p->p_sib), &(prnt->p_child));				//aggiungo il child in fondo ai fratelli 
+	p->p_parent = prnt;
+	list_add_tail(&(p->p_sib), &(prnt->p_child));				//aggiungo il child in fondo ai fratelli 
 }																	//concatenando alla lista dei figli di prnt il campo p_sib di p
 
+/* Rimuove il primo figlio del PCB prnt e lo restituisce 
+ * senza padre. Restituisce NULL se prnt non ha figli */
 pcb_t *removeChild(pcb_t *p) {
 	if(emptyChild(p))												//p non ha figli
 		return NULL;
@@ -123,32 +140,34 @@ pcb_t *removeChild(pcb_t *p) {
 	list_del(&first->p_sib);										//eliminazione del primo figlio (tolgo il collegamento ai fratelli)
 	first->p_parent = NULL;											//aggiorno il suo parent perchè non è più figlio
 	return first;		
-	}
-
+}
+	
+/* Rimuove  il PCB p dalla lista dei figli di suo padre 
+ * Se il padre è NULL restituisce NULL */
 pcb_t *outChild(pcb_t *p) {
 	if(p->p_parent == NULL) 
-		return NULL;				//p è senza padre					
-	list_del(&p->p_sib);			//elimino il puntatore (&p->p_sib) del pcb p dalla lista dei suoi fratelli (quindi elimino il pcb dalla lista dei figli del padre) 
-	p->p_parent = NULL;				//aggiorno il suo parent perchè non è più figlio 
+		return NULL;		//p è senza padre					
+	list_del(&p->p_sib);	//elimino il puntatore (&p->p_sib) del pcb p dalla lista dei suoi fratelli (quindi elimino il pcb dalla lista dei figli del padre) 
+	p->p_parent = NULL;		//aggiorno il suo parent perchè non è più figlio 
 	return p;
 }
 
-// USER defined functions
+/* USER DEFINED FUNCTIONS */
 
-// funzione inizializzazione struttura pcb_t
+/* Inizializzazione struttura del PCB */
 void inizializzapcb(struct pcb_t* p) {
-		p->p_next.next = NULL;
-		p->p_next.prev = NULL;
-		p->p_sib.next = NULL;
-		p->p_sib.prev = NULL;
-		INIT_LIST_HEAD(&p->p_child);
-		p->p_parent = NULL;
-		initPcbState(&p->p_s);
-		p->priority = 0;
-		p->p_semkey = NULL;
+	p->p_next.next = NULL;
+	p->p_next.prev = NULL;
+	p->p_sib.next = NULL;
+	p->p_sib.prev = NULL;
+	INIT_LIST_HEAD(&p->p_child);
+	p->p_parent = NULL;
+	initPcbState(&p->p_s);
+	p->priority = 0;
+	p->p_semkey = NULL;
 }
 
-// Funzione inizializzazione stato dei pcb
+/* Inizializzazione stato del PCB */
 void initPcbState(state_t* state) {
 	state->entry_hi = 0;
 	state->cause = 0;
