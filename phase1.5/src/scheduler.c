475c5cd126b3f62d48f61283fa399f0384bb5c97
mattiabiondi <mattiabiondi1@gmail.com>
2019-03-24 18:23:53 +0100

    working on scheduler

--- phase1.5/src/scheduler.c
+++ phase1.5/src/scheduler.c
@@ -1,7 +1,11 @@
+#include "const.h"
+
 void scheduler(){
   current_process = NULL;
-  if(!(emptyProcQ(&ready_queue))){
-    current_process = removeProcQ(&ready_queue);
-
+  if(!(emptyProcQ(&ready_queue))){ /* Se la coda dei processi ready non è vuota, manda in esecuzione un processo */
+    current_process = removeProcQ(&ready_queue); /* Rimozione del primo elemento di ready_queue */
+    setTIMER(TIME_SLICE); /* Settaggio del time slice */
+    LDST(&current_process->p_s); /* Esecuzione del processo tramite il caricamento del suo stato nello stato del processore */
   }
+  // WAIT(); /* Il processore va in standby ed aspetta che gli venga segnalato di riprendere l'esecuzione (tramite reset o interrupt) */
 }
