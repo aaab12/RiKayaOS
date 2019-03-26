1755c4577c0bef56469251bde08d63585befb49f
mattiabiondi <mattiabiondi1@gmail.com>
2019-03-26 13:04:09 +0100

    recursive implementation of terminate_process();

--- phase1.5/include/syscall.h
+++ phase1.5/include/syscall.h
@@ -1,6 +1,9 @@
 #ifndef __SYSCALL
 #define __SYSCALL
 
+/* Recursive Terminate Process */
+void r_termine_process(pcb_t* p);
+
 /* SYS3: termina il processo corrente e tutta la sua progenie,
  * rimuovendoli dalla ready_queue
  */
--- phase1.5/src/syscall.c
+++ phase1.5/src/syscall.c
@@ -1,15 +1,18 @@
 #include "pcb.h"
 
+void r_termine_process(pcb_t* p){
+  pcb_t* temp;
+  while(temp = removeChild(p)){ /* Estraggo i figli del processo da terminare */
+    r_termine_process(temp); /* Richiamo la funzione sul figlio */
+  }
+  outProcQ(ready_queue, p); /* Estraggo il processo dalla coda dei processi ready */
+  freePcb(current_process); /* Libero il processo */
+}
+
 /* SYS3: termina il processo corrente e tutta la sua progenie,
  * rimuovendoli dalla ready_queue
  */
 void terminate_process(){
-  pcb_t* temp1;
-  while(temp1 = removeChild(current_process)){ /* Estraggo i figli del processo da terminare */
-    pcb_t* temp2;
-    temp2 = outProcQ(ready_queue, temp1); /* Estraggo il figlio dalla coda dei processi ready */
-    if(temp2) freePcb(temp2); /* Libero il figlio */
-  }
-  freePcb(current_process); /* Libero il processo da terminare */
+  r_termine_process(current_process); /* Recursive Terminate Process */
   scheduler();
 }
