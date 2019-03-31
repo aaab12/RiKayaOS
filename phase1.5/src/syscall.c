932266be8ec5dc4eeadd2c848573657fba6d19e4
mattiabiondi <mattiabiondi1@gmail.com>
2019-03-31 18:47:38 +0200

    better recursion

--- phase1.5/src/syscall.c
+++ phase1.5/src/syscall.c
@@ -6,12 +6,12 @@
 /* Recursive Terminate Process */
 void r_termine_process(pcb_t* p){
   pcb_t* temp;
-  while((temp = removeChild(p))){ /* Estraggo i figli del processo da terminare */
-    r_termine_process(temp); /* Richiamo la funzione sul figlio */
-  }
   process_counter--;
   outProcQ(&ready_queue, p); /* Estraggo il processo dalla coda dei processi ready */
   freePcb(current_process); /* Libero il processo */
+  while((temp = removeChild(p))){ /* Estraggo i figli del processo da terminare */
+    r_termine_process(temp); /* Richiamo la funzione sul figlio */
+  }
 }
 
 /* SYS3: termina il processo corrente e tutta la sua progenie,
