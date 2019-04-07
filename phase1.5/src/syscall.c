3d76daec4a9f32911805c87d5ed255198d6ce426
mattiabiondi <mattiabiondi1@gmail.com>
2019-04-07 12:29:04 +0200

    fixed recursive terminate process

--- phase1.5/src/syscall.c
+++ phase1.5/src/syscall.c
@@ -3,12 +3,12 @@
 /* Recursive Terminate Process */
 void r_termine_process(pcb_t* p){
   pcb_t* temp;
-  process_counter--;
-  outProcQ(&ready_queue, p); /* Estraggo il processo dalla coda dei processi ready */
-  freePcb(current_process); /* Libero il processo */
   while((temp = removeChild(p))){ /* Estraggo i figli del processo da terminare */
     r_termine_process(temp); /* Richiamo la funzione sul figlio */
   }
+  process_counter--;
+  outProcQ(&ready_queue, p); /* Estraggo il processo dalla coda dei processi ready */
+  freePcb(p); /* Libero il processo */
 }
 
 /* SYS3: termina il processo corrente e tutta la sua progenie,
