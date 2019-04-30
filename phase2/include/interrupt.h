55d7f99262395ec03b27cc281a318dbe29401d62
mattiabiondi <mattiabiondi1@gmail.com>
2019-04-30 13:49:29 +0200

    files from phase1.5

---
+++ phase2/include/interrupt.h
@@ -0,0 +1,15 @@
+#ifndef _INTERRUPT_H
+#define _INTERRUPT_H
+
+#include "const.h"
+#include <umps/libumps.h>
+#include "main.h"
+#include "scheduler.h"
+#include "types_rikaya.h"
+#include "utils.h"
+
+void plt_handler();
+
+void it_handler();
+
+#endif
---
+++ phase2/src/interrupts.c
@@ -0,0 +1,22 @@
+#include "interrupt.h"
+
+extern struct list_head ready_queue;
+extern pcb_t* current_process;
+
+/* Process Local Timer handler */
+void plt_handler() {
+	state_t* caller_process = (state_t *)INT_OLDAREA;
+	save_state(caller_process, &(current_process->p_s));
+	insertProcQ(&ready_queue, current_process); /* Reinserimento del processo interrotto in stato ready */
+	setTIMER(TIME_SLICE); /* ACK */
+	scheduler();
+}
+
+/* Interval Timer handler */
+void it_handler(){
+	state_t* caller_process = (state_t *)INT_OLDAREA;
+	save_state(caller_process, &(current_process->p_s));
+	insertProcQ(&ready_queue, current_process); /* Reinserimento del processo interrotto in stato ready */
+	setIT(TIME_SLICE); /* ACK */
+	scheduler();
+}
