bf3843763d78d16c4a81604ca74e39e49b32e44f
mattiabiondi <mattiabiondi1@gmail.com>
2019-07-23 09:54:52 +0200

    comment

--- phase2/src/handler.c
+++ phase2/src/handler.c
@@ -71,7 +71,7 @@ void sysbk_handler(){
 		scheduler();
 	}
 
-	caller_process->reg_v0 = ret;
+	caller_process->reg_v0 = ret; /* Il registro v0 contiene il valore di ritorno */
 
 	user_mode(current_process); /* Il processo torna in user mode */
 	LDST(caller_process); /* Ritorniamo al flusso di esecuzione */
