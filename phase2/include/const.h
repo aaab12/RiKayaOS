019ea6116d07192fdbd5e54e5aed23b1fd1f4160
mattiabiondi <mattiabiondi1@gmail.com>
2019-07-28 14:13:06 +0200

    comment correction

--- phase2/include/const.h
+++ phase2/include/const.h
@@ -11,7 +11,7 @@
 #define EXCEPTION_STATUS 0x18000000 /* EXCEPTION STATUS: Interrupt mascherati, VM off, Kernel mode e PLT on */
 
 #define PROCESS_STATUS 0x1800FF04 /* PROCESS STATUS: Interrupt abilitati, VM off, Kernel mode e PLT on */
-// 0001 1 000 0 0 000000 01111111 00 000100
+// 0001 1 000 0 0 000000 11111111 00 000100
 // CP_U|T|V_M|x|B|xxxxxx|INT_MASK|xx|KU__IE
 
 /* SCHEDULER */
